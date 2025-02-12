b635c25a8043f5bfdd5b7f4cb9bfcac37bc6bfd8
shriya garg <shriyagarg@shriyas-MacBook-Air.local>
2025-02-13 01:44:45 +0530

    added solution to the question Print numbers from 1 to N using recursion instead of loops. The function should recursively call itself, decreasing N until it reaches 1, printing values in increasing order.

---
+++ 240648_Shriyagarg_L1_Q3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void printNumbers(int n) {
+    if (n == 0){
+        return;
+    }
+    printNumbers(n - 1);  
+    printf("%d ", n);
+}
+
+int main() {
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    printNumbers(n);  
+    return 0;
+}
